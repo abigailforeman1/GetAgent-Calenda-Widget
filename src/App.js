b308543e586ea0c8409949acd2829feaff3fef6c
abigailforeman <forea005@gmail.com>
2020-06-12 16:50:32 +0100

    stored current date, got basic outline of calendar, added current dat to top of calendar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import moment from 'moment'
 
-const App = () => {
-  return (
-    <h1>Hello World</h1>
-  )
+class App extends React.Component {
+  state = {
+    currentDate: '',
+    daysInMonth: null,
+    months: moment.months()
+  }
+
+  componentDidMount () {
+    const currentDate = moment()
+    const daysInMonth = parseInt(moment(currentDate).daysInMonth())
+    this.setState({ currentDate, daysInMonth })
+  }
+
+  render () {
+    if (!this.state.currentDate) return null
+
+    console.log(this.state.selectedDay)
+
+    const displayDate = this.state.currentDate._d.toString()
+    const slicedYear = displayDate.slice(10, 15)
+    const slicedMonth = displayDate.slice(3, 8)
+
+    return (
+      <>
+        <div className='main-wrapper'>
+          <div className='outer-border '>
+            <div className='row'>
+              <div className='current-year-month-wrapper'>
+                <div className='column'>
+                  <h1 className='current-year-month'>
+                    {slicedMonth} {slicedYear}
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App
